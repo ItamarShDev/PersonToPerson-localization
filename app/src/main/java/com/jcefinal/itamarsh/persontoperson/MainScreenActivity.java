2953c618f82b345f2b0c75e531df459985d8b5fe
itamar sharify <itamarsharify@gmail.com>
2015-11-28 17:30:45 +0200

    close issue #6 added basic gui

--- app/src/main/java/com/jcefinal/itamarsh/persontoperson/MainScreenActivity.java
+++ app/src/main/java/com/jcefinal/itamarsh/persontoperson/MainScreenActivity.java
@@ -1,7 +1,13 @@
 package com.jcefinal.itamarsh.persontoperson;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -16,7 +22,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -29,6 +45,12 @@ public class MainScreenActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    private TabLayout tab;
+    public int[] colorIntArray = {R.color.blue, R.color.red};
+    public int[] colorIntArray2 = {R.color.dark_blue, R.color.dark_pink};
+    public int[] iconIntArray = {R.drawable.ic_add_white_24dp,R.drawable.ic_stop_white_24dp};
+    private FloatingActionButton fab;
+    private Toolbar toolbar;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -40,19 +62,19 @@ public class MainScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mViewPager = (ViewPager) findViewById(R.id.container);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
+        mSectionsPagerAdapter.addFragment(new PlaceholderFragment());
+        mSectionsPagerAdapter.addFragment(new PlaceholderFragment());
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //FLOATING BUTTON
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setBackgroundColor(getResources().getColor(R.color.blue));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,9 +83,65 @@ public class MainScreenActivity extends AppCompatActivity {
             }
         });
 
+        tab = (TabLayout) findViewById(R.id.tabs);
+        tab.setupWithViewPager(mViewPager);
+        tab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+                animateFab(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
     }
 
+    protected void animateFab(final int position) {
+        fab.clearAnimation();
+        // Scale down animation
+        ScaleAnimation shrink =  new ScaleAnimation(1f, 0.2f, 1f, 0.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        shrink.setDuration(150);     // animation duration in milliseconds
+        shrink.setInterpolator(new DecelerateInterpolator());
+        shrink.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Change FAB color and icon
+                fab.setBackgroundTintList(getResources().getColorStateList(colorIntArray[position]));
+                toolbar.setBackgroundColor(getResources().getColor(colorIntArray2[position]));
+                tab.setBackgroundColor(getResources().getColor(colorIntArray2[position]));
+                fab.setImageDrawable(getResources().getDrawable(iconIntArray[position]));
+
+                // Scale up animation
+                ScaleAnimation expand =  new ScaleAnimation(0.2f, 1f, 0.2f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                expand.setDuration(100);     // animation duration in milliseconds
+                expand.setInterpolator(new AccelerateInterpolator());
+                fab.startAnimation(expand);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        fab.startAnimation(shrink);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -89,11 +167,15 @@ public class MainScreenActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
+    //the activities themselves
     public static class PlaceholderFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
+        private Cursor cursor;
+        private DAL dal;
+        private SimpleCursorAdapter cursorAdapter;
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
@@ -117,15 +199,27 @@ public class MainScreenActivity extends AppCompatActivity {
             View rootView;
             if(getArguments().getInt(ARG_SECTION_NUMBER)==1){
                 rootView = inflater.inflate(R.layout.content_contacts, container, false);
+                ListView cursorListView = (ListView)rootView.findViewById(R.id.cursorListView);
+                dal = new DAL(this.getActivity());
+                String[] entries = new String[] {Contacts.ContactsTable.userID};
+                int [] viewsID = new int[] {R.id.userNameTextView};
+                cursor = dal.getAllTimeEntriesCursor();
+                cursorAdapter = new SimpleCursorAdapter(this.getActivity(), R.layout.contact, cursor, entries, viewsID, 0);
+                cursorListView.setAdapter(cursorAdapter);
+                return rootView;
+
             }
-            if(getArguments().getInt(ARG_SECTION_NUMBER)==2)
+            if(getArguments().getInt(ARG_SECTION_NUMBER)==2){
                 rootView = inflater.inflate(R.layout.fragment_add_page, container, false);
+                return rootView;
+            }
             else {
                 rootView = inflater.inflate(R.layout.fragment_main_screen, container, false);
                 TextView textView = (TextView) rootView.findViewById(R.id.section_label);
                 textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            }
                 return rootView;
+
+            }
         }
     }
 
@@ -134,7 +228,9 @@ public class MainScreenActivity extends AppCompatActivity {
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
+        private final List<Fragment> FragmentList = new ArrayList();
+        private final List<String> FragmentTitles = new ArrayList();
+        private int tabCount = 2;
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
@@ -145,22 +241,26 @@ public class MainScreenActivity extends AppCompatActivity {
             // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
         }
-
+        public void addFragment(Fragment fragment) {
+            FragmentList.add(fragment);
+        }
+        public void setTabCount(int num){
+            this.tabCount = num;
+        }
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return tabCount;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Contacts";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "Search";
+//                case 2:
+//                    return "SECTION 3";
             }
             return null;
         }
